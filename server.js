a2ca8fccbd9c3ae29eabe8b565cb798f29759b72
ltebean <yucong1118@gmail.com>
2013-12-23 09:59:13 +0800

    add ci server

--- server.js
+++ server.js
@@ -1,17 +1,3 @@
- var Client = require('ftp');
-  var fs = require('fs');
-
-  var c = new Client();
-  c.on('ready', function() {
-  	console.log("ready");
-    c.put('/product-shop-web/', 'foo.remote-copy.txt', function(err) {
-      if (err) throw err;
-      c.end();
-    });
-  });
-  // connect to localhost:21 as anonymous
-  c.connect({
-  	host:"10.1.1.81",
-  	user:"qaupload",
-  	password:"uploadwar"
-  });
+require("./bin/wizard-ci.js").serve({
+      port:3000
+});
